599a7b4a7c833a2e297ddb35c200a1226edd95d1
Hunterly01 <mhunterly@gmail.com>
2025-12-01 22:24:08 +0100

    some done

---
+++ src/main/java/com/indrive/dtos/requets/AdminRequests/RegisterDriverRequest.java
@@ -0,0 +1,11 @@
+package com.indrive.dtos.requets.AdminRequests;
+
+
+public class RegisterDriverRequest {
+    private String name;
+    private String username;
+    private String email;
+    private String plateNumber;
+    private String address;
+    private boolean activeStatus;
+}
