6e0d99e7372086b20f81b8dd9a3c6e7cd0d48502
Hunterly01 <mhunterly@gmail.com>
2025-12-02 09:19:24 +0100

    done here again

--- src/main/java/com/indrive/services/PassengerServiceImpl.java
+++ src/main/java/com/indrive/services/PassengerServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PassengerServiceImpl implements PassengerService {
+public class PassengerServiceImpl imgitlements PassengerService {
 
     @Autowired
     private PassengerRepository repository;
