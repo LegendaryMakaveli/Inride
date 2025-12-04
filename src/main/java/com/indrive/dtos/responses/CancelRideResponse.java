238ed011c9c0e5ee43d9388e92ba4f779e110543
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-04 11:53:28 +0100

    update cancel ride response

--- src/main/java/com/indrive/dtos/responses/CancelRideResponse.java
+++ src/main/java/com/indrive/dtos/responses/CancelRideResponse.java
@@ -2,7 +2,10 @@ package com.indrive.dtos.responses;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class CancelRideResponse {
     private String rideRequestId;
+    private String dateOFCancellation;
 }
